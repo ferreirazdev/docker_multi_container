54d5ff71c0d3e3e95c70eec2475eaa4e54694ad0
ferreirazdev <business.flavioferreira@gmail.com>
2023-02-09 08:52:13 -0300

    feat: connect the containers in the same docker network

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-docker run --name mongodb -d --rm -p 27017:27017 mongo
+docker network create todo-net
+docker run --network todo-net --name mongodb -d --rm -p 27017:27017 mongo
 docker build -t todoapp ./backend
-docker run --name todo-app -d todoapp
+docker run --network todo-net --rm --name todo-app -d todoapp
