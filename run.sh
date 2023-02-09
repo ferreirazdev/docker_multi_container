18ea722d189e48ff767b8079151b484f99a34b26
ferreirazdev <business.flavioferreira@gmail.com>
2023-02-09 09:23:50 -0300

    feat: dockerizing react app

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 docker network create todo-net
-docker run --network todo-net --name mongodb -d --rm -p 27017:27017 mongo
+docker run --network todo-net --rm --name mongodb -d  -p 27017:27017 mongo
 docker build -t todoapp ./backend
-docker run --network todo-net --rm --name todo-app -d todoapp
+docker run --network todo-net --rm --name todo-app -d -p 8000:8000 todoapp
+docker build -t frontend ./frontend
+docker run -it --rm --name frontend-todo -d -p 3000:3000 frontend
